e69181e7bbc492b706e112850d5cc86c36760f14
shivaansh81001 <shivaans@ualberta.ca>
2024-12-15 17:41:56 -0700

    Adding files

---
+++ main.py
@@ -0,0 +1,14 @@
+from softmax import *
+from CNN import *
+from softmax import *
+from one_by_k import *
+
+
+
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
---
+++ softmax.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 17:33:42 2024
+
+@author: shiva
+"""
+
